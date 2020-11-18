a561effe1cd94ab18329a3a144527e8bbe53cb2a
André <tdc@gmx.ch>
2020-11-18 19:10:43 +0100

    fix deployment script for new client path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
+cd client
 npm install
 npm run build
+cd ..
 ~/go/bin/bee pack -be "-tags prod" -exr="^(?:node_modules)$"
 cp goPriceWatch.tar.gz /var/www/keramic.ch/goPriceWatch.tar.gz
 cd /var/www/keramic.ch/ 
