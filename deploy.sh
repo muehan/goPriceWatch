422ac96cb7e50c5419e1c993cbaffed73750421a
André <tdc@gmx.ch>
2020-11-18 20:23:28 +0100

    ignore client during beego build

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ cd client
 npm install
 npm run build
 cd ..
-~/go/bin/bee pack -be "-tags prod" -exr="^(?:node_modules)$"
+~/go/bin/bee pack -be "-tags prod" -exr="^(?:client)$"
 cp goPriceWatch.tar.gz /var/www/keramic.ch/goPriceWatch.tar.gz
 cd /var/www/keramic.ch/ 
 tar -xvzf *.tar.gz
